beee8abda23d68530b33a1c3743a991301050b11
Anastasiya Tkachuk <118803480+AnastasiyaTkachuk00@users.noreply.github.com>
2023-04-24 21:34:11 +0300

    Web-tests Zoobazar.by

---
+++ conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope="session")
+def browser():
+    driver = webdriver.Chrome("./chromedriver")
+    yield driver
+    driver.quit()
+
